249d90d1c67d795fc27304f5f8a8003f725cae56
sunmeehuh <sunmee725@gmail.com>
2015-01-07 19:20:03 -0500

    Bower_components, cities folder

---
+++ app/bower_components/angular-route/_vti_cnf/angular-route.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|05 Jan 2015 05:42:10 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|Home-PC\\Home
+vti_modifiedby:SR|Home-PC\\Home
+vti_timecreated:TR|05 Jan 2015 05:42:10 -0000
+vti_cacheddtm:TX|05 Jan 2015 05:42:10 -0000
+vti_filesize:IR|33054
+vti_backlinkinfo:VX|qpx_test/vulture-master-master/app/index.html
+vti_syncwith_planetickets.com\:21:TX|05 Jan 2015 05:42:10 -0000
+vti_syncofs_planetickets.com\:21:TW|05 Jan 2015 06:36:04 -0000
